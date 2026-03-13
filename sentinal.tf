ecf908778192ebf01df19e00a5c386126af59bfd
Akshay S A <akshaysa595@gmail.com>
2026-03-13 15:00:15 +0530

    Add AWS provider and EC2 instance configuration

--- sentinal.tf
+++ sentinal.tf
@@ -1,1 +1,12 @@
-helo madhusudan
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+
+   tags = {
+    Name = "HelloWorld"
+  }  
+}
